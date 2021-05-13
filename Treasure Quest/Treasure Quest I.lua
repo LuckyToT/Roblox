2f09bf529cf3864596db3833dcd36bfea1dce84c
LuckyToT <75943344+LuckyToT@users.noreply.github.com>
2021-05-13 21:06:46 +0700

    Update Treasure Quest I.lua

--- Treasure Quest/Treasure Quest I.lua
+++ Treasure Quest/Treasure Quest I.lua
@@ -1,20 +1,24 @@
+-- if you lag it's not script it's obfuscator make the script lag
 -- Dungeon Setting --
-_G.Dungeon = 'Ancient Jungle' -- Ancient Jungle,Coral Kingdom,Crystal Cave,Sacred Sands,Candy Land,Sinister Sewers,Magical Mushrooms,Space Base,Daring Dojo,Cryptic Castle,Santa's Workshop,Ice Palace,Frenzy Factory,Fiery Fortress
+_G.AutoDungeon = true -- auto choose dungeon and difficulty (not support Monolith Boss yet)
+_G.Dungeon = "Ancient Jungle" -- Ancient Jungle,Coral Kingdom,Crystal Cave,Sacred Sands,Candy Land,Sinister Sewers,Magical Mushrooms,Space Base,Daring Dojo,Cryptic Castle,Santa's Workshop,Ice Palace,Frenzy Factory,Fiery Fortress, Danger Depths, Poison Prison,
 _G.Difficulty = 'Easy' -- Easy,Medium,Hard,Demon,Impossible
 _G.GameMode = 'Classic' -- Classic,Uncapped,Endless,Endless Uncapped,Minibosses
 _G.Privacy = 'Solo' --Solo,Friends
 
 -- Misc --
+_G.Instakill = true -- it make your game lag and sometime it will break the game
+
 _G.DestroyMap = true
 _G.AutoMedKit = true
 _G.AutoChest = true
 
 -- Webhook --
-_G.Webhook = { -- sometime it's broken
-    Enabled = true, -- true/false
+_G.Webhook = { -- sometime it's broken (when using Instakill in "Endless/Endless Uncapped" gamemode)
+    Enabled = false, -- true/false
     webhookLink = 'https://discord.com/api/webhooks/',
     PingForRarity = {
-        ['Common'] = true,
+        ['Common'] = false,
         ['Rare'] = true, 
         ['Legendary'] = true, 
         ['Mythical'] = true, 
@@ -22,9 +26,9 @@ _G.Webhook = { -- sometime it's broken
     },
 }
 
--- Auto Sell
+-- Auto Sell --
 _G.AutoSell = { -- be careful with this shit autosell
-    Enabled = true, -- true/false
+    Enabled = false, -- true/false
     ['Common'] = true, 
     ['Rare'] = false, 
     ['Legendary'] = false, 
@@ -32,6 +36,4 @@ _G.AutoSell = { -- be careful with this shit autosell
     ['Ultimate'] = false
 }
 
--- AutoUpgrade, AutoEquip ?```
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/LuckyToT/Roblox/main/Treasure%20Quest/Treasure%20Quest%20II.lua", true))()
+-- AutoUpgrade, AutoEquip ?
