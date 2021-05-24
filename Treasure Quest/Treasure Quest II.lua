05b381e66279b2854a8793508ad321e05bdaa220
LuckyToT <75943344+LuckyToT@users.noreply.github.com>
2021-05-24 21:29:23 +0700

    Update Treasure Quest II.lua

--- Treasure Quest/Treasure Quest II.lua
+++ Treasure Quest/Treasure Quest II.lua
@@ -1,1 +1,1 @@
-game.Players.LocalPlayer:Kick('Stop using this script. \nyou have chance to get ban(someone got banned). \nby ! LuckyToT#6102')
+game.Players.LocalPlayer:Kick('Stop using this script. \nyou have chance to get ban(bruh someone got banned). \nby ! LuckyToT#6102')
