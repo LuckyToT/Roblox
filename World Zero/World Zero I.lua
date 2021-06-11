7b9afa9e5dfc84d26696bf58f0f29f29c9a06349
LuckyToT <75943344+LuckyToT@users.noreply.github.com>
2021-06-11 19:40:20 +0700

    Update World Zero I.lua

--- World Zero/World Zero I.lua
+++ World Zero/World Zero I.lua
@@ -1,2 +1,3 @@
 -- Don't say the script patched if you're using FREE EXPLOIT
+-- Dont attack the monster during the kill aura is enabled you will get kick
 loadstring(game:HttpGet("https://raw.githubusercontent.com/LuckyToT/Roblox/main/World%20Zero/World%20Zero%20GUI.lua", true))()
