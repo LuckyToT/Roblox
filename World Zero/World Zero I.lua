95b40ade60dd8b59c3c81abefedd644a35ff3590
LuckyToT <75943344+LuckyToT@users.noreply.github.com>
2021-06-11 17:04:38 +0700

    Update World Zero I.lua

--- World Zero/World Zero I.lua
+++ World Zero/World Zero I.lua
@@ -1,2 +1,2 @@
--- Dont say the script patched if you using FREE EXPLOIT
+-- Don't say the script patched if you're using FREE EXPLOIT
 loadstring(game:HttpGet("https://raw.githubusercontent.com/LuckyToT/Roblox/main/World%20Zero/World%20Zero%20GUI.lua", true))()
