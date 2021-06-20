5600e3ffd694f772592de1e6ff8164165390e563
LuckyToT <75943344+LuckyToT@users.noreply.github.com>
2021-06-20 18:30:43 +0700

    Update World Zero I.lua

--- World Zero/World Zero I.lua
+++ World Zero/World Zero I.lua
@@ -1,3 +1,1 @@
--- Don't say the script patched if you're using FREE EXPLOIT
--- Don't attack mob or spam ability during kill aura is enabled you will get kick
-loadstring(game:HttpGet("https://raw.githubusercontent.com/LuckyToT/Roblox/main/World%20Zero/World%20Zero%20GUI.lua", true))()
+game.Players.LocalPlayer:Kick('Discord: https://discord.gg/qEqcFQQqeA')
